d780ff5c4b8716a17220bedf27855c3519d0d5a6
Márcio <marcionitao@gmail.com>
2024-02-11 12:07:22 +0100

    update vote if user want to changer option

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -1,6 +1,7 @@
 import z from 'zod'
 import { randomUUID } from 'node:crypto' // para gerar um UUID aleatório e unico
 import { FastifyInstance } from 'fastify'
+import { prisma } from '../../lib/prisma'
 
 export async function voteOnPoll(app: FastifyInstance) {
   // usuario irá votar em uma enquete especifica
@@ -19,6 +20,33 @@ export async function voteOnPoll(app: FastifyInstance) {
     // verificar se dentro da minha sessionId existe o cookie
     let { sessionId } = request.cookies
 
+    // caso o cookie exista, verificar se o usuario já votou nesta enquete
+    if (sessionId) {
+      const userPreviousVoteOnPoll = await prisma.vote.findUnique({
+        where: {
+          sessionId_pollId: {
+            sessionId,
+            pollId,
+          },
+        },
+      })
+      if (
+        userPreviousVoteOnPoll &&
+        userPreviousVoteOnPoll.pollOptionId !== pollOptionId
+      ) {
+        // apagar o voto anterior
+        await prisma.vote.delete({
+          where: {
+            id: userPreviousVoteOnPoll.id,
+          },
+        })
+      } else if (userPreviousVoteOnPoll) {
+        return reply
+          .status(408)
+          .send({ message: 'You already voted on this poll' })
+      }
+    }
+    // caso o cookie nao exista, criar um novo
     if (!sessionId) {
       // resolvendo a fiabilidade da opção que o usuario irá votar(usando cookies)
       sessionId = randomUUID()
@@ -29,6 +57,15 @@ export async function voteOnPoll(app: FastifyInstance) {
         httpOnly: true, // para que o cookie seja acessível somente pelo servidor
       })
     }
+
+    // criando o voto no banco de dados
+    await prisma.vote.create({
+      data: {
+        sessionId,
+        pollId,
+        pollOptionId,
+      },
+    })
 
     // 201 - significa que o registo foi criado com sucesso
     return reply.status(201).send()
